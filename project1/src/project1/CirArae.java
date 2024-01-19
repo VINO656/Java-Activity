ba87df5dc82b7a8f462e1cdbbed1ca40a7bc5838
VINO656 <vinothini6684@gmail.com>
2024-01-19 06:20:04 +0530

    initial commit

---
+++ project1/src/project1/CirArae.java
@@ -0,0 +1,15 @@
+package project1;
+
+//Java program to calculate the area of the
+public class CirArae{
+	public static void main(String[] args)
+	{
+		int radius;
+		double pi = 3.142, area;
+		radius = 5;
+		// calculating the area of the circle
+		area = pi * radius * radius;
+		// printing the area of the circle
+		System.out.println("Area of circle is :" + area);
+	}
+}
